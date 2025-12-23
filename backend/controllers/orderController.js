ef7e03e4cb1f207785d3ac8abf3e92fbf80351da
ajaycoder01 <ajaywebmaster01@gmail.com>
2025-12-24 02:31:36 +0530

    Change frontend URL to production environment
    Updated frontend URL for placing user orders.

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -8,7 +8,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 // placing user order for fronted----
 const placeOrder = async (req, res) =>{
 
-    const fronted_url = "http://localhost:5173";
+    const fronted_url = "https://food-delivery-frontend-h2da.onrender.com";
 
     try {
         const newOrder = new orderModel({
